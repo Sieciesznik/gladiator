ab5bafb31162bfbf15898571d365ef0825c83362
gzyniu <grzesiekmaj@o2.pl>
2019-02-06 00:30:47 +0100

    added Welcome Screen with nickname input

--- Gladiator/Gladiator/gamemachine.h
+++ Gladiator/Gladiator/gamemachine.h
@@ -85,26 +85,42 @@ void GameMachine::run() {
 
 void WelcomeScreenState::run() {
 	
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!!");
-
+	sf::RenderWindow window(sf::VideoMode(1900, 1000), "SFML works!!");
 	sf::Texture texture;
+	sf::Event event;
+	sf::Sprite WelcomeScreen;
+	sf::String nickName;
+	sf::Font font;
+	font.loadFromFile("arial.ttf");
+	sf::Text playerText(nickName, font, 50);
+	playerText.setPosition(450,550);
+	playerText.setColor(sf::Color::White);
+
 	if (!texture.loadFromFile("Images/welcome_screen.png"))
 	{
 		LOG_ERROR("Can't load the file!");
 	}
-	sf::Sprite WelcomeScreen;
+
 	WelcomeScreen.setTexture(texture);
+
 	while (window.isOpen())
 	{
-		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::TextEntered)
+			{
+				if (event.text.unicode >= 32 && event.text.unicode <= 126 && nickName.getSize() < 20)
+					nickName += (char)event.text.unicode;
+				else if (event.text.unicode == 8 && nickName.getSize() > 0)
+					nickName.erase(nickName.getSize() - 1, nickName.getSize());
+				playerText.setString(nickName);
+			}
 		}
-
 		window.clear();
 		window.draw(WelcomeScreen);
+		window.draw(playerText);
 		window.display();
 	}
 }
